034ce1c05a483146838144d3ee3522e2f832f911
kphutiyagae <kopanophutiyagae@gmail.com>
2024-03-19 07:42:57 +0200

    feat(none): Added routing and nested routing

---
+++ src/app/articles/latest/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LatestArticles() {
+    return (
+        <div>List of latest articles</div>
+    );
+}
+
+export default LatestArticles;
---
+++ src/app/articles/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Articles() {
+    return (
+        <div>Hello From Articles</div>
+    );
+}
+
+export default Articles;
